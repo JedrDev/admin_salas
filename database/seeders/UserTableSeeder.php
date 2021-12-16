edf94c288015f81044aceb022946cd636ec4fdd3
JedrDev <78862420+JedrDev@users.noreply.github.com>
2021-12-16 09:54:31 -0600

    Modelos y controladores, instalacion de laravel UI

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@salastest.com',
+            'password' => bcrypt('password123'), // password123
+            'type' => 'ADMIN',
+        ]);
+    }
+}
